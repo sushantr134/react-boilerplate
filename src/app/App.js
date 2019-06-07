d3236ad534db7c52c618edc2b0f7df9c590b0ee7
Sushant Singh <sushantr134@gmail.com>
2019-06-08 00:16:01 +0530

    Jest Enzyme Testing Framework added

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./app-global.module.scss";
 
+import Sushant from "./components/sushant/component";
+
 const App = () => (
   <React.Fragment>
-    <h1 className={styles.heading}>My Name is sushant</h1>
+    <h1 className={styles.heading}>Scss modules working...</h1>
+    <Sushant />
   </React.Fragment>
 );
 export default App;
---
+++ src/app/components/sushant/component.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+//import component that need to be test
+import Sushant from "./component";
+
+//defining component
+describe("Sushant Component", () => {
+  beforeEach(() => {
+    //This method runs before test and will be use for initialisation of props data and other parameters required in the test
+  });
+
+  it("It should render without errors", () => {
+    //defining tests
+    const component = shallow(<Sushant />);
+    console.log(component.debug());
+    const wrapper = component.find(`[data-test='heading']`);
+    expect(wrapper.length).toBe(1);
+  });
+});
