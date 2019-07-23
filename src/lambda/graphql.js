b7336aec5b2373025a56551aa68f889b91c1e3d3
Sushant Singh <sushantr134@gmail.com>
2019-07-23 16:47:55 +0530

    playground production ready

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -29,7 +29,9 @@ const resolvers = {
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  introspection: true,
+  playground: true
 });
 
 exports.handler = server.createHandler({
